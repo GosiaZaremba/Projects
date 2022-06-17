ceae58114e5d6d0ac7efb972063e6f0a13002c56
Gosia Zaremba <gosia@szel.net>
2022-06-17 21:32:22 +0200

    Added timestamp for all activities.

--- React_Native/DogNanny/screens/PetPanel.js
+++ React_Native/DogNanny/screens/PetPanel.js
@@ -5,8 +5,47 @@ import { Button } from "../components/Button";
 import { PetPhoto } from "../components/PetPhoto";
 import { PanelButton } from "../components/PanelButton";
 import { Colors } from "../constants/colors";
+import { useState } from "react";
 
 export const PetPanel = () => {
+  const [walksHistory, setWalksHistory] = useState([]);
+  const [feedingHistory, setFeedingHistory] = useState([]);
+  const [pillsHistory, setPillsHistory] = useState([]);
+  const [playsHistory, setPlaysHistory] = useState([]);
+
+  const onPressWalk = () => {
+    setWalksHistory([new Date().toLocaleTimeString(), ...walksHistory]);
+    if (walksHistory.length >= 3) {
+      setWalksHistory([new Date().toLocaleTimeString(), ...walksHistory]);
+      setWalksHistory([walksHistory.pop()]);
+    }
+    console.log("walks", walksHistory);
+  };
+
+  const onPressFood = () => {
+    setFeedingHistory([new Date().toLocaleTimeString(), ...feedingHistory]);
+    if (feedingHistory.length >= 3) {
+      setFeedingHistory([new Date().toLocaleTimeString(), ...feedingHistory]);
+      setFeedingHistory([feedingHistory.pop()]);
+    }
+    console.log("food", feedingHistory);
+  };
+  const onPressPills = () => {
+    setPillsHistory([new Date().toLocaleTimeString(), ...pillsHistory]);
+    if (pillsHistory.length >= 3) {
+      setPillsHistory([new Date().toLocaleTimeString(), ...pillsHistory]);
+      setPillsHistory([pillsHistory.pop()]);
+    }
+    console.log("pills", pillsHistory);
+  };
+  const onPressPlays = () => {
+    setPlaysHistory([new Date().toLocaleTimeString(), ...playsHistory]);
+    if (playsHistory.length >= 3) {
+      setPlaysHistory([new Date().toLocaleTimeString(), ...playsHistory]);
+      setPlaysHistory([playsHistory.pop()]);
+    }
+    console.log("plays", playsHistory);
+  };
   return (
     <View>
       <View>
@@ -14,29 +53,56 @@ export const PetPanel = () => {
       </View>
       <Card>
         <PetPhoto photoUrl={require("../assets/7.jpg")} />
-        <View styles={styles.innerContainer}>
-          <View style={styles.buttonContainer}>
-            <PanelButton buttonType={require("../assets/images/feed.png")} />
-            <PanelButton buttonType={require("../assets/images/pills.png")} />
-            <PanelButton buttonType={require("../assets/images/play.png")} />
-            <PanelButton buttonType={require("../assets/images/poop.png")} />
+        <View style={styles.innerContainer}>
+          <PanelButton
+            buttonType={require("../assets/images/feed.png")}
+            onPress={onPressFood}
+          />
+          <View style={styles.dataView}>
+            {feedingHistory.map((food) => (
+              <Text key={food} style={styles.dataText}>
+                {food}
+              </Text>
+            ))}
           </View>
-          <View style={styles.dataContainer}>
-            <View style={styles.dataView}>
-              <Text>Today 9:13</Text>
-              <Text>Today 13:29</Text>
-              <Text>Today 17:45</Text>
-            </View>
-            <View style={styles.dataView}>
-              <Text>Today 9:13</Text>
-              <Text>Today 13:29</Text>
-              <Text>Today 17:45</Text>
-            </View>
-            <View style={styles.dataView}>
-              <Text>Today 9:13</Text>
-              <Text>Today 13:29</Text>
-              <Text>Today 17:45</Text>
-            </View>
+        </View>
+        <View style={styles.innerContainer}>
+          <PanelButton
+            buttonType={require("../assets/images/poop.png")}
+            onPress={onPressWalk}
+          />
+          <View style={styles.dataView}>
+            {walksHistory.map((walk) => (
+              <Text key={walk} style={styles.dataText}>
+                {walk}
+              </Text>
+            ))}
+          </View>
+        </View>
+        <View style={styles.innerContainer}>
+          <PanelButton
+            buttonType={require("../assets/images/pills.png")}
+            onPress={onPressPills}
+          />
+          <View style={styles.dataView}>
+            {pillsHistory.map((pill) => (
+              <Text key={pill} style={styles.dataText}>
+                {pill}
+              </Text>
+            ))}
+          </View>
+        </View>
+        <View style={styles.innerContainer}>
+          <PanelButton
+            buttonType={require("../assets/images/play.png")}
+            onPress={onPressPlays}
+          />
+          <View style={styles.dataView}>
+            {playsHistory.map((play) => (
+              <Text key={play} style={styles.dataText}>
+                {play}
+              </Text>
+            ))}
           </View>
         </View>
       </Card>
