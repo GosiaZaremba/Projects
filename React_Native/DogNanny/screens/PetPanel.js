d0459897ad2075d330d0f59bdeba9890e426a71c
Gosia Zaremba <gosia@szel.net>
2022-06-17 17:06:42 +0200

    Working on pet panel, changed styles so it looks even.

--- React_Native/DogNanny/screens/PetPanel.js
+++ React_Native/DogNanny/screens/PetPanel.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, ScrollView } from "react-native";
 import { Logo } from "../components/Logo";
 import { Card } from "../components/Card";
 import { PetPhoto } from "../components/PetPhoto";
@@ -6,45 +6,55 @@ import { PanelButton } from "../components/PanelButton";
 import { Colors } from "../constants/colors";
 
 export const PetPanel = () => {
+  const data = {
+    walks: [],
+    food: [],
+    pills: [],
+    play: [],
+  };
+  const onPressWalk = () => {
+    data.walks.push();
+  };
+
   return (
-    <View style={styles.outerContainer}>
+    <ScrollView style={styles.outerContainer}>
       <Logo />
       <Card>
         <PetPhoto photoUrl={require("../assets/7.jpg")} />
         <View style={styles.innerContainer}>
           <PanelButton buttonType={require("../assets/images/feed.png")} />
           <View style={styles.dataView}>
-            <Text>Today 9:13</Text>
-            <Text>Today 13:29</Text>
-            <Text>Today 17:45</Text>
+            <Text style={styles.dataText}>Today 9:13</Text>
+            <Text style={styles.dataText}>Today 13:29</Text>
+            <Text style={styles.dataText}>Today 17:45</Text>
           </View>
         </View>
         <View style={styles.innerContainer}>
           <PanelButton buttonType={require("../assets/images/poop.png")} />
           <View style={styles.dataView}>
-            <Text>Today 9:13</Text>
-            <Text>Today 13:29</Text>
-            <Text>Today 17:45</Text>
+            <Text style={styles.dataText}>Today 9:13</Text>
+            <Text style={styles.dataText}>Today 13:29</Text>
+            <Text style={styles.dataText}>Today 17:45</Text>
           </View>
         </View>
         <View style={styles.innerContainer}>
           <PanelButton buttonType={require("../assets/images/pills.png")} />
           <View style={styles.dataView}>
-            <Text>Today 9:13</Text>
-            <Text>Today 13:29</Text>
-            <Text>Today 17:45</Text>
+            <Text style={styles.dataText}>Today 9:13</Text>
+            <Text style={styles.dataText}>Today 13:29</Text>
+            <Text style={styles.dataText}>Today 17:45</Text>
           </View>
         </View>
         <View style={styles.innerContainer}>
           <PanelButton buttonType={require("../assets/images/play.png")} />
           <View style={styles.dataView}>
-            <Text>Today 9:13</Text>
-            <Text>Today 13:29</Text>
-            <Text>Today 17:45</Text>
+            <Text style={styles.dataText}>Today 9:13</Text>
+            <Text style={styles.dataText}>Today 13:29</Text>
+            <Text style={styles.dataText}>Today 17:45</Text>
           </View>
         </View>
       </Card>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -63,5 +73,9 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary.medium,
     borderRadius: 15,
     padding: 10,
+    margin: 5,
+  },
+  dataText: {
+    color: Colors.secondary.light,
   },
 });
