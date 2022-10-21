76939353af7e910a37874b2182830e45cd6e5ca1
Gosia Zaremba <gosia@szel.net>
2022-10-21 16:39:05 +0200

    Added refs for pet form

--- React_Native/petnanny/acomponents/organisms/PetForm/PetForm.tsx
+++ React_Native/petnanny/acomponents/organisms/PetForm/PetForm.tsx
@@ -1,51 +1,82 @@
-import React from 'react';
+import { getVersion } from 'jest';
+import React, { useRef } from 'react';
 import { View } from 'react-native';
-import { CustomButton, CustomInput } from '../../atoms';
-import { AppDatePicker } from '../../molecules';
+import { CustomButton, CustomInput, CustomInputReference } from '../../atoms';
+import {
+    AppDatePicker,
+    AppDatePickerReference,
+    AppImagePicker,
+    AppImagePickerReference,
+} from '../../molecules';
 
 export type Props = {};
 
 export const PetForm: React.FC<Props> = () => {
+    const nameInputReference = useRef<CustomInputReference>(null);
+    const breedInputReference = useRef<CustomInputReference>(null);
+    const descriptionInputReference = useRef<CustomInputReference>(null);
+    const dateInputReference = useRef<AppDatePickerReference>(null);
+    const imageInputReference = useRef<AppImagePickerReference>(null);
+
     const onPressSubmit = () => {
-        console.log('click');
+        const nameValue = nameInputReference.current?.getValue() || '';
+        const breedValue = breedInputReference.current?.getValue() || '';
+        const dateValue = dateInputReference.current?.getValue() || '';
+        const descriptionValue =
+            descriptionInputReference.current?.getValue() || '';
+        const imageValue = imageInputReference.current?.getValue() || '';
+        // console.log('nameValue', nameValue);
+        // console.log('breedValue', breedValue);
+        // console.log('dateValue', dateValue);
+        // console.log('descriptionValue', descriptionValue);
+        // console.log('imageValue', imageValue);
     };
     return (
         <View>
-            <CustomInput
-                placeholder={'Name'}
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType={'name'}
-                autoCapitalize={'words'}
-                secureTextEntry={false}
-                multiline={false}
-                maxLength={20}
-            />
-            <CustomInput
-                placeholder={'Breed'}
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType={'breed'}
-                autoCapitalize={'words'}
-                secureTextEntry={false}
-                multiline={false}
-                maxLength={20}
-            />
-            <AppDatePicker placeholder={'DateOfBirth'} />
-            <CustomInput
-                placeholder={'Name'}
-                autoCorrect={false}
-                spellCheck={false}
-                textContentType={'name'}
-                autoCapitalize={'words'}
-                secureTextEntry={false}
-                multiline={false}
-                maxLength={20}
-            />
-            <CustomButton
-                buttonTitle={'Submit'}
-                onPressButton={onPressSubmit}
-            />
+            <AppImagePicker ref={imageInputReference} />
+            <View>
+                <CustomInput
+                    placeholder={'Name'}
+                    autoCorrect={false}
+                    spellCheck={false}
+                    textContentType={'name'}
+                    autoCapitalize={'words'}
+                    secureTextEntry={false}
+                    multiline={false}
+                    maxLength={20}
+                    ref={nameInputReference}
+                />
+                <CustomInput
+                    placeholder={'Breed'}
+                    autoCorrect={false}
+                    spellCheck={false}
+                    textContentType={'breed'}
+                    autoCapitalize={'words'}
+                    secureTextEntry={false}
+                    multiline={false}
+                    maxLength={20}
+                    ref={breedInputReference}
+                />
+                <AppDatePicker
+                    placeholder={'DateOfBirth'}
+                    ref={dateInputReference}
+                />
+                <CustomInput
+                    placeholder={'Description'}
+                    autoCorrect={false}
+                    spellCheck={false}
+                    textContentType={'name'}
+                    autoCapitalize={'words'}
+                    secureTextEntry={false}
+                    multiline={true}
+                    maxLength={140}
+                    ref={dateInputReference}
+                />
+                <CustomButton
+                    buttonTitle={'Submit'}
+                    onPressButton={onPressSubmit}
+                />
+            </View>
         </View>
     );
 };
